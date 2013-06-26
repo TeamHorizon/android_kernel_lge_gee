892d8f372f7e6f06d2c7bf91471002a8f54ca092
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2013-06-26 15:35:06 +0530

    msm: rotator: Extend fast YUV invalid checker for 2-pass
    For 2-pass mode, we need to check for the fast YUV constraints
    separately for both the 1st and 2nd pass. Make change to extend
    the fast YUV invalid checker to 2-pass mode.
    
    CRs-fixed: 504007
    Change-Id: I7ef96ff29423fbd5cc454e8fd125b4bb1a9d59e1
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/char/msm_rotator.c
+++ drivers/char/msm_rotator.c
@@ -2414,8 +2414,10 @@ static int msm_rotator_start(unsigned long arg,
 	case MDP_Y_CRCB_H2V2_TILE:
 	case MDP_Y_CBCR_H2V2_TILE:
 		if (rotator_hw_revision >= ROTATOR_REVISION_V2) {
-			fast_yuv_en = !fast_yuv_invalid_size_checker(
-						info.rotations,
+			if (info.downscale_ratio &&
+					(info.rotations & MDP_ROT_90)) {
+				fast_yuv_en = !fast_yuv_invalid_size_checker(
+						0,
 						info.src.width,
 						info.src_rect.w >>
 							info.downscale_ratio,
@@ -2437,6 +2439,17 @@ static int msm_rotator_start(unsigned long arg,
 						dst_h,
 						dst_w,
 						is_planar420);
+			} else {
+				fast_yuv_en = !fast_yuv_invalid_size_checker(
+						info.rotations,
+						info.src.width,
+						dst_w,
+						info.src.height,
+						dst_h,
+						dst_w,
+						is_planar420);
+			}
+
 			if (fast_yuv_en && info.downscale_ratio &&
 				(info.rotations & MDP_ROT_90))
 					enable_2pass = 1;
